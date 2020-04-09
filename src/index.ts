61a42eb029f261a2a6f3c592553d7d3062481f5a
esnode85 <esnodejs@gmail.com>
2020-04-09 20:20:53 +0200

    set-up the backend with typeorm

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import "reflect-metadata";
+import App from "./app";
+import PostsController from "./controllers/posts.controller";
+
+const app = new App([new PostsController()], 5000);
+
+app.listen();
